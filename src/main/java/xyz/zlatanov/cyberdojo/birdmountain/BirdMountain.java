e9569fe4956ad82947671ed85fb32311e8678e17
Boyan Zlatanov <b.zlatanov@tinqin.com>
2025-02-16 21:07:08 +0000

    Bird mountain

--- src/main/java/xyz/zlatanov/cyberdojo/birdmountain/BirdMountain.java
+++ src/main/java/xyz/zlatanov/cyberdojo/birdmountain/BirdMountain.java
@@ -2,9 +2,9 @@ package xyz.zlatanov.cyberdojo.birdmountain;
 
 public class BirdMountain {
 
-	private static final int	UNESTIMATED	= -1;
+	private static final int	NOT_ESTIMATED	= -1;
 	private final int[][]		grid;
-	private int					height		= 0;
+	private int					height			= 0;
 
 	public BirdMountain(String view) {
 		var viewSplit = view.split("\n");
@@ -17,7 +17,7 @@ public class BirdMountain {
 				if (c == ' ') {
 					grid[y][x] = 0;
 				} else {
-					grid[y][x] = UNESTIMATED;
+					grid[y][x] = NOT_ESTIMATED;
 				}
 			}
 		}
@@ -33,23 +33,27 @@ public class BirdMountain {
 			var line = grid[y];
 			for (int x = 0; x < line.length; x++) {
 				var spot = line[x];
-				if (spot == UNESTIMATED && hasEstimatedNeighbour(y, x)) {
+				if (spot == NOT_ESTIMATED && hasEstimatedNeighbour(y, x)) {
 					grid[y][x] = height + 1;
 				}
 			}
 		}
-		updateHeight();
+		var updated = updateHeight();
+		if (updated) {
+			estimateHeights();
+		}
 	}
 
 	private boolean hasEstimatedNeighbour(int y, int x) {
+		var edge = height == 0 && (x == 0 || y == 0 || x == grid[0].length - 1 || y == grid.length - 1);
 		var left = x > 0 && grid[y][x - 1] == height;
 		var right = x < grid[0].length - 1 && grid[y][x + 1] == height;
 		var top = y > 0 && grid[y - 1][x] == height;
 		var bottom = y < grid.length - 1 && grid[y + 1][x] == height;
-		return left || right || top || bottom;
+		return edge || left || right || top || bottom;
 	}
 
-	private void updateHeight() {
+	private boolean updateHeight() {
 		var max = 0;
 		for (int[] line : grid) {
 			for (int i : line) {
@@ -58,6 +62,8 @@ public class BirdMountain {
 				}
 			}
 		}
+		var updated = max != height;
 		height = max;
+		return updated;
 	}
 }
--- src/test/java/xyz/zlatanov/cyberdojo/birdmountain/BirdMountainTest.java
+++ src/test/java/xyz/zlatanov/cyberdojo/birdmountain/BirdMountainTest.java
@@ -15,4 +15,36 @@ class BirdMountainTest {
 	void shouldEstimateSingleHill() {
 		assertEquals(1, new BirdMountain(" ^ ").height());
 	}
+
+	@Test
+	void shouldEstimateDoubleHill() {
+		assertEquals(2, new BirdMountain("""
+				^^^
+				^^^
+				^^^""").height());
+	}
+
+	@Test
+	void shouldPassKataExample() {
+		assertEquals(3, new BirdMountain("" +
+				"^^^^^^       \n" +
+				" ^^^^^^^^    \n" +
+				"  ^^^^^^^    \n" +
+				"  ^^^^^      \n" +
+				"  ^^^^^^^^^^^\n" +
+				"  ^^^^^^     \n" +
+				"  ^^^^       \n").height());
+	}
+
+	@Test
+	void shouldEstimateX() {
+		assertEquals(2, new BirdMountain("" +
+				"xxx   xxx\n" +
+				" xxx xxx \n" +
+				"  xxxxx  \n" +
+				"   xxx   \n" +
+				"  xxxxx  \n" +
+				" xxx xxx \n" +
+				"xxx   xxx\n").height());
+	}
 }
